adec3a9ef913ec01ba1761a0a1cf2a70c8e79ca5
ROBERTO CRUZADO MARTINEZ <Roberto.Cruzado@alu.uclm.es>
2022-03-20 18:59:30 +0100

    mysqli_connect

--- eclipse-workspace/algoritmosKLW/index.php
+++ eclipse-workspace/algoritmosKLW/index.php
@@ -1,2 +1,4 @@
 <?php
-echo("hola munddo");
+$conBBDD=@mysqli_connect(localhost,root,nokiace6,dos_dksbasico)or die("Error en la conexion");
+$cadenasConNombreUsuario = mysqli_query($conBBDD,"SELECT Clave FROM conceptos");
+echo("hola");

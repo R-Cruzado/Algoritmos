6a1ab9f9d35b6c1e2649787296ca8832c8eb2f44
ROBERTO CRUZADO MARTINEZ <Roberto.Cruzado@alu.uclm.es>
2022-03-20 19:39:55 +0100

    Conexión con BBDD

--- eclipse-workspace/algoritmosKLW/index.php
+++ eclipse-workspace/algoritmosKLW/index.php
@@ -1,4 +1,14 @@
 <?php
-$conBBDD=@mysqli_connect(localhost,root,nokiace6,dos_dksbasico)or die("Error en la conexion");
-$cadenasConNombreUsuario = mysqli_query($conBBDD,"SELECT Clave FROM conceptos");
-echo("hola");
+
+
+$conBBDD=@mysqli_connect("localhost","root","nokiace6","dos_dksbasico")or die("Error en la conexion");
+
+$cadenasConNombreUsuario = mysqli_query($conBBDD,"select Clave from conceptos");
+    
+while ($row = mysqli_fetch_assoc($cadenasConNombreUsuario))
+    print $row['Clave'];
+
+mysqli_close($conBBDD);
+
+
+?>
